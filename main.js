7392120a4d583ee87af498546bd8bd2014e6fc1f
druizoses <druizoses@gmail.com>
2016-01-31 10:09:57 +0100

    Led example

--- main.js
+++ main.js
@@ -6,55 +6,48 @@ io = require('socket.io')(server);
 var SerialPort = require("serialport");
 var serialport = SerialPort.SerialPort;
 
-// list serial ports:
 
 app.use(express.static('public'));		
 
 var brightness = 0;
 
+//Every page create a socket connection.
 io.sockets.on('connection', function (socket) {
-	console.log('Alguien se ha conectado con Sockets');
+	
+	//Socket used in Led.html
+  	var brightness = 0;
 	socket.on('led', function (data) {
 		brightness = data.value;
 		
 		var buf = new Buffer(1);
 		buf.writeUInt8(brightness, 0);
-		//serialPort.write(buf);
+		myPort.write(buf);
 		
-			
+		io.sockets.emit('led', {value: brightness});	
 	});
-		socket.on('newmessage', function (socket) {
-	sockets.emit('message', socket);
-	console.log(socket);
-});
-socket.on('connectToPort', function (portName) {
+
+	//Every page try to connect searching the Arduinos USB port. 
+	socket.on('connectToPort', function (portName) {
 	
 
-	 myPort = new serialport(portName, {
-		baudRate: 9600,
-		// look for return and newline at the end of each data packet:
+		myPort = new serialport(portName, {
+			baudRate: 9600,		
+		});
 		
- });
-  myPort.on("open", function () {
-  console.log('open');
-});
-  myPort.on('data', function(data) {
-    console.log('data received: ' + data);
-	io.sockets.emit('message', data.toString());
-  });
-  
+		myPort.on("open", function () {
+		console.log('open');
+		});
+		
+		myPort.on('data', function(data) {
+		console.log('data received: ' + data);
+		io.sockets.emit('message', data.toString());
+		});
+
+	});
 
 });
 
-
-});
-
-
-
-
-console.log("running");
-
-
+//Every page run a GET ajax request to ask for the avaliable ports.
 app.get('/getPorts', function(req, res){
 	listPorts = [];
 	SerialPort.list(function (err, ports) {
